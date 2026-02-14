5f27166165ea36ba9149427139965fb2a74c1433
delihop <syn82829@gmail.com>
2026-02-14 23:51:11 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import './style.css'
-import ufo from './ufo.png.PNG'
 
 function App() {
   return (
     <div className="app">
 
-      {/* логотип */}
       <h1 className="logo">UFOmo</h1>
 
-      {/* glass crash блок */}
       <div className="crash-panel">
 
         <img
-          src={ufo}
-          alt="UFO"
+          src="/src/ufo.png.PNG"
           className="ufo-image"
         />
 
