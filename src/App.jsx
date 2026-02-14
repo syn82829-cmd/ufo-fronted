e88e1054436437fa4c277b0ece182031687f6a3f
delihop <syn82829@gmail.com>
2026-02-14 21:29:00 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>UFO</h1>
+      <p className="subtitle">Cases • Roulette • Drops</p>
+
+      <div className="card">
+        <p>🎁 Open your first case</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
