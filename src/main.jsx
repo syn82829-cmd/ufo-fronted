55eeafc30df64e5b8dbf23e7972ba6d07122f558
delihop <syn82829@gmail.com>
2026-02-17 03:42:39 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,24 @@ import App from './App'
 if (window.Telegram) {
   window.Telegram.WebApp.ready()
   window.Telegram.WebApp.expand()
+
+  const user = window.Telegram.WebApp.initDataUnsafe?.user
+
+  if (user) {
+    fetch('https://ufo-backend-1.onrender.com/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: user.id,
+        username: user.username
+      })
+    })
+    .then(res => res.json())
+    .then(data => console.log('User synced:', data))
+    .catch(err => console.error('Sync error:', err))
+  }
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
