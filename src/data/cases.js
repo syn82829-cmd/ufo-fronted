1751a1fd594cc54f859866676f2b2804a1403884
delihop <syn82829@gmail.com>
2026-02-19 01:19:31 +0300

    Update cases.js

--- src/data/cases.js
+++ src/data/cases.js
@@ -1,1 +1,20 @@
-
+export const cases = {
+  darkmatter: {
+    id: "darkmatter",
+    name: "Dark Matter",
+    drops: [
+      { id: "darkhelmet", chance: 5 },
+      { id: "gift", chance: 10 },
+      { id: "westside", chance: 15 },
+      { id: "lowrider", chance: 20 },
+      { id: "watch", chance: 20 },
+      { id: "skull", chance: 25 },
+      { id: "dyson", chance: 30 },
+      { id: "batman", chance: 8 },
+      { id: "poizon", chance: 18 },
+      { id: "metla", chance: 22 },
+      { id: "ball", chance: 35 },
+      { id: "book", chance: 40 }
+    ]
+  }
+};
