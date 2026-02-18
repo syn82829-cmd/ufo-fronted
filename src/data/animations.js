e3d28728d141703105cae1d19e1c44542e6d0673
delihop <syn82829@gmail.com>
2026-02-19 01:21:11 +0300

    Create animations.js

---
+++ src/data/animations.js
@@ -0,0 +1,27 @@
+import darkhelmet from "@/public/animations/darkmatter/darkhelmet.json";
+import gift from "@/public/animations/darkmatter/gift.json";
+import westside from "@/public/animations/darkmatter/westside.json";
+import lowrider from "@/public/animations/darkmatter/lowrider.json";
+import watch from "@/public/animations/darkmatter/watch.json";
+import skull from "@/public/animations/darkmatter/skull.json";
+import dyson from "@/public/animations/darkmatter/dyson.json";
+import batman from "@/public/animations/darkmatter/batman.json";
+import poizon from "@/public/animations/darkmatter/poizon.json";
+import metla from "@/public/animations/darkmatter/metla.json";
+import ball from "@/public/animations/darkmatter/ball.json";
+import book from "@/public/animations/darkmatter/book.json";
+
+export const darkMatterAnimations = {
+  darkhelmet,
+  gift,
+  westside,
+  lowrider,
+  watch,
+  skull,
+  dyson,
+  batman,
+  poizon,
+  metla,
+  ball,
+  book
+};
