f4da591dc77607df5014670e16db8dd75edf93e4
delihop <syn82829@gmail.com>
2026-02-21 23:33:14 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import "./style.css"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import ErrorBoundary from "./ErrorBoundary"
 
-if (window.Telegram?.WebApp) {
+if (window.Telegram && window.Telegram.WebApp) {
   window.Telegram.WebApp.ready()
   window.Telegram.WebApp.expand()
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 )
