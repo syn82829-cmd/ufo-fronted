18f597605aa77a92a8e03bb49413d62763da3a8a
delihop <syn82829@gmail.com>
2026-02-25 01:32:16 +0300

    Update cases.js

--- src/data/cases.js
+++ src/data/cases.js
@@ -2,20 +2,20 @@ export const cases = {
   darkmatter: {
     id: "darkmatter",
     name: "Dark Matter",
-    image: "/cases/case3.png.PNG", // ← ВАЖНО: добавили image сюда
+    image: "/cases/case3.png.PNG",
     drops: [
-      { id: "darkhelmet", chance: 5 },
-      { id: "gift", chance: 10 },
-      { id: "westside", chance: 15 },
-      { id: "lowrider", chance: 20 },
-      { id: "watch", chance: 20 },
-      { id: "skull", chance: 25 },
-      { id: "dyson", chance: 30 },
-      { id: "batman", chance: 8 },
-      { id: "poizon", chance: 18 },
-      { id: "metla", chance: 22 },
-      { id: "ball", chance: 35 },
-      { id: "book", chance: 40 }
+      { id: "darkhelmet", chance: 5,  png: "HeroicHelmet" },
+      { id: "gift",       chance: 10, png: "LootBag" },
+      { id: "westside",   chance: 15, png: "WestsideSign" },
+      { id: "lowrider",   chance: 20, png: "Lowrider" },
+      { id: "watch",      chance: 20, png: "SwissWatch" },
+      { id: "skull",      chance: 25, png: "skull" },
+      { id: "dyson",      chance: 30, png: "IonicDryer" },
+      { id: "batman",     chance: 8,  png: "batman" },
+      { id: "poizon",     chance: 18, png: "poison" }, // да, id с опечаткой оставляем, png нормальный
+      { id: "metla",      chance: 22, png: "metla" },
+      { id: "ball",       chance: 35, png: "ball" },
+      { id: "book",       chance: 40, png: "book" }
     ]
   }
 }
