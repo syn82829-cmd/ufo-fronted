28eb5fc2f0816bf1c5c04033df6d2b5c6a25b846
delihop <syn82829@gmail.com>
2026-03-07 02:24:19 +0300

    Update CaseHeader.jsx

--- src/components/case/CaseHeader.jsx
+++ src/components/case/CaseHeader.jsx
@@ -3,15 +3,9 @@ function CaseHeader({
   isSpinning,
   resultDrop,
   imgRef,
-  wrapRef,
-  lineRef,
-  reelRef,
-  reelItems,
-  dropMap,
   navigate,
   openCase,
   phase,
-  pngSrcByDrop,
 }) {
   return (
     <div className="casepage-header">
@@ -38,29 +32,6 @@ function CaseHeader({
           className={`casepage-case-image ${isSpinning ? "hidden-case" : ""}`}
           alt={caseData.name}
         />
-
-        {isSpinning && (
-          <div ref={wrapRef} className="roulette-window">
-            <div ref={lineRef} className="roulette-line" />
-            <div ref={reelRef} className="roulette-reel">
-              {reelItems.map((dropId, index) => {
-                const drop = dropMap[dropId]
-                if (!drop) return null
-
-                return (
-                  <div key={index} className="roulette-item" data-index={index}>
-                    <img
-                      src={pngSrcByDrop(drop)}
-                      className="roulette-png"
-                      alt=""
-                      draggable={false}
-                    />
-                  </div>
-                )
-              })}
-            </div>
-          </div>
-        )}
       </div>
 
       {!resultDrop && (
