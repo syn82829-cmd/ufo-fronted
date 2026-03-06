6230ce6e914e5a6fa50dbfba3f7dbe7d2104a6d4
delihop <syn82829@gmail.com>
2026-03-06 23:59:55 +0300

    Update CaseCard.jsx

--- src/components/CaseCard.jsx
+++ src/components/CaseCard.jsx
@@ -1,50 +1,35 @@
 import { useNavigate } from "react-router-dom"
 
 function CaseCard({ caseItem }) {
-
   const navigate = useNavigate()
 
   function openCase() {
-
     navigate(`/case/${caseItem.id}`, {
-      state: caseItem
+      state: caseItem,
     })
-
   }
 
   return (
-
-    <div
-      className="case-card"
-      onClick={openCase}
-    >
-
+    <div className="case-card" onClick={openCase}>
       {/* CASE NAME */}
-
       <div className="case-title">
         {caseItem.name}
       </div>
 
-
       {/* CASE IMAGE */}
-
       <img
         src={caseItem.image}
         className="case-image"
         alt={caseItem.name}
       />
 
-
       {/* PRICE */}
-
       <div className="case-price-badge">
-        ⭐ {caseItem.price}
+        <img src="/ui/star.jpg" className="case-price-icon" alt="" />
+        <span>{caseItem.price}</span>
       </div>
-
     </div>
-
   )
-
 }
 
 export default CaseCard
