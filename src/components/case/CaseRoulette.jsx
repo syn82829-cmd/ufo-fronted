567f1fd2aba71001608ed958d77784665a6c954c
delihop <syn82829@gmail.com>
2026-03-07 02:15:20 +0300

    Update CaseRoulette.jsx

--- src/components/case/CaseRoulette.jsx
+++ src/components/case/CaseRoulette.jsx
@@ -1,1 +1,36 @@
+function CaseRoulette({
+  isSpinning,
+  wrapRef,
+  lineRef,
+  reelRef,
+  reelItems,
+  dropMap,
+  pngSrcByDrop,
+}) {
+  if (!isSpinning) return null
 
+  return (
+    <div ref={wrapRef} className="roulette-window">
+      <div ref={lineRef} className="roulette-line" />
+      <div ref={reelRef} className="roulette-reel">
+        {reelItems.map((dropId, index) => {
+          const drop = dropMap[dropId]
+          if (!drop) return null
+
+          return (
+            <div key={index} className="roulette-item" data-index={index}>
+              <img
+                src={pngSrcByDrop(drop)}
+                className="roulette-png"
+                alt=""
+                draggable={false}
+              />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default CaseRoulette
