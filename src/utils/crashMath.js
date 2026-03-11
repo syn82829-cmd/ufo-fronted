7a3bfe118b3971c2c241a03de12e50701472fa74
delihop <syn82829@gmail.com>
2026-03-11 15:27:22 +0300

    Update crashMath.js

--- src/utils/crashMath.js
+++ src/utils/crashMath.js
@@ -1,1 +1,6 @@
+export const CRASH_WAITING_MS = 5000
 
+export function getMultiplierByElapsedMs(elapsedMs) {
+  const elapsed = Math.max(0, elapsedMs) / 1000
+  return +Math.exp(0.14 * elapsed).toFixed(2)
+}
