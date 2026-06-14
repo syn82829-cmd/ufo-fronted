cbf85f48b94c5b0fbff60512d0f04813d5546458
delihop <syn82829@gmail.com>
2026-06-14 21:00:21 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,7 +35,7 @@ function Home() {
     { id: "randomcase", image: "/cases/case8.png.PNG", name: "Random Case", price: 499, free: false },
     { id: "singularity", image: "/cases/singularity.PNG", name: "Singularity", price: 150, free: false },
     { id: "matrix", image: "/cases/matrix.PNG", name: "Matrix", price: 100, free: false },
-    { id: "milkyway", image: "/cases/milkyway.PNG", name: "Milky Way", price: 50, free: false },
+    { id: "bigbang", image: "/cases/bigbang.PNG", name: "Big Bang", price: 50, free: false },
     { id: "3friends", image: "/cases/3friends.PNG", name: "For 3 Friends", price: null, free: true },
   ]
 
