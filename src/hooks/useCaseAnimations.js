d14d1645933f3a2abd6534845f84be41b4c9746d
delihop <syn82829@gmail.com>
2026-06-27 18:50:01 +0300

    Make legacy case animation hook inert

--- src/hooks/useCaseAnimations.js
+++ src/hooks/useCaseAnimations.js
@@ -1,54 +1,5 @@
-import { useEffect, useState } from "react"
-
-function useCaseAnimations(drops, activeDropId) {
-  const [animationsById, setAnimationsById] = useState({})
-
-  useEffect(() => {
-    let cancelled = false
-
-    async function loadActiveAnimation() {
-      if (!activeDropId) {
-        setAnimationsById({})
-        return
-      }
-
-      const activeDrop = (drops || []).find((drop) => drop?.id === activeDropId)
-
-      if (!activeDrop?.lottie) {
-        setAnimationsById({})
-        return
-      }
-
-      try {
-        const res = await fetch(activeDrop.lottie)
-        if (!res.ok) {
-          throw new Error(`Failed to load ${activeDrop.lottie}`)
-        }
-
-        const json = await res.json()
-
-        if (!cancelled) {
-          setAnimationsById({
-            [activeDrop.id]: json,
-          })
-        }
-      } catch (err) {
-        console.error(`LOTTIE LOAD ERROR [${activeDrop.id}]`, err)
-
-        if (!cancelled) {
-          setAnimationsById({})
-        }
-      }
-    }
-
-    loadActiveAnimation()
-
-    return () => {
-      cancelled = true
-    }
-  }, [drops, activeDropId])
-
-  return animationsById
+function useCaseAnimations() {
+  return {}
 }
 
 export default useCaseAnimations
